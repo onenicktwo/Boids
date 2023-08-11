73c4bbc53ad0992c81726d346e67e74159dc1c6f
TehLily <tehlily@gmail.com>
2023-08-10 19:47:33 -0500

    Adding Grass Background

---
+++ Boids/Assets/Scripts/Support/ChangeMusic.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMusic : MonoBehaviour {
+
+    public AudioSource gameMusic;
+    public AudioSource pauseMusic;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartPause() {
+        gameMusic.Stop();
+        pauseMusic.Play();
+    }
+
+    public void EndPause() {
+        pauseMusic.Stop();
+        gameMusic.Play();
+    }
+}
